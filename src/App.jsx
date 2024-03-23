c6c320bec8bb948fe6b922601111c33c6074b702
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-03-24 00:15:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SimpleForm from './components/SimpleForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleForm.jsx
@@ -0,0 +1,13 @@
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br />
+                <input type="submit" value="Submite" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
